27c5b6e1acd99cc9ded3c54450e3b01dc9c27c1f
JorgeQ <alejandrojj2018@gmail.com>
2020-11-13 05:55:55 -0500

    Agregando seccion de header y seccion de top + img y estilos

---
+++ main.js
@@ -0,0 +1,20 @@
+// evento menu 
+/*
+const $btnMenu = document.getElementById('btn-menu'),
+$menu = document.querySelectorAll('div')
+$ctnMenu = document.querySelectorAll('.ctn-icon-menu')
+$iconMenu = document.querySelectorAll('.icon-menu')
+$a = document.querySelectorAll('a')
+$img = document.querySelectorAll('img')
+
+
+function hideMenu (){
+$iconMenu.forEach(el => {
+    $iconMenu.querySelectorAll('img')
+});
+}
+
+$btnMenu.addEventListener('click', (e) =>{
+    hideMenu()
+})
+*/
