afbbc4a866c8607ece3521781ee13ba94ebade97
JorgeQ <alejandrojj2018@gmail.com>
2020-11-22 02:17:51 -0500

    Agregando Funcionalidad a mi boton Menu y agregando estilos a mi logo con js

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 // evento menu 
-/*
-const $btnMenu = document.getElementById('btn-menu'),
-$menu = document.querySelectorAll('div')
-$ctnMenu = document.querySelectorAll('.ctn-icon-menu')
-$iconMenu = document.querySelectorAll('.icon-menu')
-$a = document.querySelectorAll('a')
-$img = document.querySelectorAll('img')
+const $ctnbuscar = document.querySelector('.buscar')
+const $ctnRedesYmas = document.querySelector('.redesYmas')
+const $ctnlogo = document.querySelector('.ctn-title')
+const $logoTitleDark = document.querySelector('.ctn-textTitle h3')
+const $logoTitleRed = document.querySelector('.ctn-textTitle h3 span')
 
+const disableSeccionesHeader  = () => {
+    $ctnbuscar.style.display = 'none';
+    $ctnRedesYmas.style.display = 'none'
 
-function hideMenu (){
-$iconMenu.forEach(el => {
-    $iconMenu.querySelectorAll('img')
-});
+    $ctnlogo.style.margin = '-7% 2%'
+    $ctnlogo.style.marginBottom = '7%'
+
+    $logoTitleDark.style.color = '#D81F1F'
+    $logoTitleRed.style.color = 'black'
 }
 
-$btnMenu.addEventListener('click', (e) =>{
-    hideMenu()
-})
-*/
+const enableSeccionesHeader = () => {
+    $ctnbuscar.style.display = 'flex';
+    $ctnRedesYmas.style.display = 'flex'
+
+    $ctnlogo.style.margin = '0% 4%'
+
+    $logoTitleDark.style.color = 'black'
+    $logoTitleRed.style.color = '#D81F1F'
+
+
+} 
+
+document.addEventListener('click', (e) =>{
+    console.log(e.target)
+
+    
+    if (e.target.matches('#btn-menu')){
+        if ($ctnbuscar.style.display === 'flex' || $ctnRedesYmas.style.display === 'flex') {
+            disableSeccionesHeader()
+        }
+        else{
+            enableSeccionesHeader()
+        }
+    }
+
+});
